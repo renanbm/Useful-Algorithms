01a66a793e1eeeba89d7f3e7f07477e52181fc5a
Renan Ben Moshe <renan.moshe@gmail.com>
2016-09-25 17:08:26 -0300

    Merge Sort

---
+++ MergeSort.py
@@ -0,0 +1,35 @@
+def MergeSort(list):
+    n = len(list)
+    if n < 2:
+        return
+    mid = n//2
+    lefthalf = list[:mid]
+    righthalf = list[mid:]
+    MergeSort(lefthalf)
+    MergeSort(righthalf)
+
+    nL = len(lefthalf)
+    nR = len(righthalf)
+
+    i = 0
+    j = 0
+    k = 0
+
+    while i < nL and j < nR:
+        if lefthalf[i] <= righthalf[j]:
+            list[k] = lefthalf[i]
+            i += 1
+        else:
+            list[k] = righthalf[j]
+            j += 1
+        k += 1
+
+    while i < nL:
+        list[k] = lefthalf[i]
+        k += 1
+        i += 1
+
+    while j < nR:
+        list[k] = righthalf[j]
+        k += 1
+        j += 1
